11b883c2f8091ff53b516242c4df5db472467013
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-12 08:16:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
   row_index = 0
   while row_index < directors_database[0][:movies].length do 
     print directors_database[0][:movies][row_index][:title]
+    row_index += 1
   end
-  row_index += 1
 end
 
